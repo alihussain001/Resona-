600234656e42a6a428b8aa8d2c6d09a58e7b6675
alihussain <ahalihussain11@gmail.com>
2026-03-05 22:24:13 +0500

    setup api environment and added api url

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports:[HttpModule],
   controllers: [SongsController],
   providers: [SongsService]
 })
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,6 +1,10 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SongsService {
-    
+    constructor(private readonly httpService: HttpService,
+        private readonly configService: ConfigService,
+    ){}   
 }
